3a6acbbf557a87706ef5df74a82b4baa836230ed
Paweł Kulpa <pawel.kulpa@ovoo.pl>
2022-07-14 09:44:13 +0200

    Add distributor test 3001blocks.

--- x/cferoutingdistributor/abci_test.go
+++ x/cferoutingdistributor/abci_test.go
@@ -167,3 +167,63 @@ func TestBurningWithInflationDistributor(t *testing.T) {
 	coinOnRemainAccount := app.CferoutingdistributorKeeper.GetAccountCoinsForModuleAccount(ctx, "remains")
 	require.EqualValues(t, sdk.NewInt(2), coinOnRemainAccount.AmountOf(denom))
 }
+
+func TestBurningWithInflationDistributorAfter3001Blocks(t *testing.T) {
+	perms := []string{authtypes.Minter, authtypes.Burner}
+	collector := "fee_collector"
+	denom := "uc4e"
+	inflationCollector := "c4e_distributor"
+	testapp.AddMaccPerms(collector, perms)
+	testapp.AddMaccPerms(inflationCollector, perms)
+	app := testapp.Setup(false)
+
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	routingDistibutor := prepareBurningDistributor()
+	subDistributors := append(routingDistibutor.SubDistributor, prepareInflationSubDistributor())
+	routingDistibutor.SubDistributor = subDistributors
+
+	app.CferoutingdistributorKeeper.SetRoutingDistributor(ctx, routingDistibutor)
+
+	//coin from fee 1017 * 3000
+	for i := 1; i <= 3001; i++ {
+		//prepare module account with coin to distribute fee_collector 1017
+
+		cointToMint := sdk.NewCoin(denom, sdk.NewInt(1017))
+		app.BankKeeper.MintCoins(ctx, collector, sdk.NewCoins(cointToMint))
+
+		//prepare coin minted from inflation 899
+		cointToMintFromInflation := sdk.NewCoin(denom, sdk.NewInt(5044))
+		app.BankKeeper.MintCoins(ctx, inflationCollector, sdk.NewCoins(cointToMintFromInflation))
+		ctx = ctx.WithBlockHeight(int64(i))
+		app.BeginBlocker(ctx, abci.RequestBeginBlock{})
+		app.EndBlocker(ctx, abci.RequestEndBlock{})
+	}
+
+	ctx = ctx.WithBlockHeight(int64(3002))
+	app.BeginBlocker(ctx, abci.RequestBeginBlock{})
+	app.EndBlocker(ctx, abci.RequestEndBlock{})
+
+	coinRemains := app.CferoutingdistributorKeeper.GetRoutingDistributorr(ctx).RemainsMap["c4e_distributor"].LeftoverCoin
+	require.EqualValues(t, sdk.MustNewDecFromStr("0.33"), coinRemains)
+
+	burnRemains := app.CferoutingdistributorKeeper.GetRoutingDistributorr(ctx).RemainsMap["burn"].LeftoverCoin
+	require.EqualValues(t, sdk.MustNewDecFromStr("0.670000000000000000"), burnRemains)
+
+	acc, _ := sdk.AccAddressFromBech32("c4e1p20lmfzp4g9vywl2jxwexwh6akvkxzpae05wyk")
+	developmentFundAccount := app.CferoutingdistributorKeeper.GetAccountCoins(ctx, acc)
+	require.EqualValues(t, sdk.MustNewDecFromStr("1720635"), developmentFundAccount.AmountOf(denom).ToDec())
+
+	coinRemainsDevelopmentFund := app.CferoutingdistributorKeeper.GetRoutingDistributorr(ctx).RemainsMap["c4e1p20lmfzp4g9vywl2jxwexwh6akvkxzpae05wyk"].LeftoverCoin
+
+	require.EqualValues(t, sdk.MustNewDecFromStr("0.4354"), coinRemainsDevelopmentFund)
+
+	validatorRewardCollectorAccountCoin := app.CferoutingdistributorKeeper.GetAccountCoinsForModuleAccount(ctx, "validators_rewards_collector")
+	require.EqualValues(t, sdk.MustNewDecFromStr("0"), validatorRewardCollectorAccountCoin.AmountOf(denom).ToDec())
+
+	coinRemainsValidatorsReward := app.CferoutingdistributorKeeper.GetRoutingDistributorr(ctx).RemainsMap["validators_rewards_collector"].LeftoverCoin
+	require.EqualValues(t, sdk.MustNewDecFromStr("0.5646"), coinRemainsValidatorsReward)
+
+	coinOnRemainAccount := app.CferoutingdistributorKeeper.GetAccountCoinsForModuleAccount(ctx, "remains")
+	require.EqualValues(t, sdk.NewInt(2), coinOnRemainAccount.AmountOf(denom))
+}
