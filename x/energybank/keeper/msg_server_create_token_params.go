6952e275881fe352e382b6dc2a46e8a11d8ceabe
dawid <dawid.kruk@ovoo.pl>
2022-08-10 10:33:36 +0200

    Change keepers of energybank. Migration of EnergyBank module finished (successful)

--- x/energybank/keeper/msg_server_create_token_params.go
+++ x/energybank/keeper/msg_server_create_token_params.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/chain4energy/c4e-chain/x/energybank/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,8 +11,23 @@ import (
 func (k msgServer) CreateTokenParams(goCtx context.Context, msg *types.MsgCreateTokenParams) (*types.MsgCreateTokenParamsResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	var token = types.TokenParams{
+		Index:          msg.Name,
+		Name:           msg.Name,
+		TradingCompany: msg.TradingCompany,
+		BurningTime:    msg.BurningTime,
+		BurningType:    msg.BurningType,
+		SendPrice:      msg.SendPrice,
+		MintAccount:    msg.Creator,
+	}
+
+	_, isFound := k.GetTokenParams(ctx, token.Name)
+
+	if isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Token with this name already exists")
+	}
+
+	k.SetTokenParams(ctx, token)
 
 	return &types.MsgCreateTokenParamsResponse{}, nil
 }
