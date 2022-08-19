035806ec332a8f7ee61b4d5a02db8b4d7840ce88
Paweł Kulpa <pawel.kulpa@ovoo.pl>
2022-08-19 09:13:27 +0200

    Add checking share sum is proper.

--- x/cferoutingdistributor/types/sub_distributor.go
+++ x/cferoutingdistributor/types/sub_distributor.go
@@ -7,7 +7,7 @@ import (
 
 func (s SubDistributor) Validate() error {
 
-	if CheckPercentShareSumIsLowerThen100(s.Destination) {
+	if CheckPercentShareSumIsGTEThen100(s.Destination) {
 		return fmt.Errorf("share sum is greater or equal 100")
 	}
 
@@ -26,14 +26,17 @@ func (s SubDistributor) Validate() error {
 	return nil
 }
 
-func CheckPercentShareSumIsLowerThen100(destination Destination) bool {
+func CheckPercentShareSumIsGTEThen100(destination Destination) bool {
 	shares := destination.Share
 	percentShareSum := sdk.MustNewDecFromStr("0")
 	for _, share := range shares {
 		percentShareSum = percentShareSum.Add(share.Percent)
 	}
 
-	percentShareSum.Add(destination.BurnShare.Percent)
+	if destination.BurnShare != nil {
+
+		percentShareSum = percentShareSum.Add(destination.BurnShare.Percent)
+	}
 
 	if percentShareSum.GTE(sdk.MustNewDecFromStr("100")) {
 		return true
