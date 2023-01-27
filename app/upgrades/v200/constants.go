f7ebb4502ee83a97209a31705be4956a9cc8fd97
Paweł Kulpa <pawel.kulpa@ovoo.pl>
2023-01-27 12:44:55 +0100

    Change migration to v2.0.0 delete old migration (we dont need this).

--- app/upgrades/v200/constants.go
+++ app/upgrades/v200/constants.go
@@ -1,11 +1,11 @@
-package v101
+package v200
 
 import (
 	"github.com/chain4energy/c4e-chain/app/upgrades"
 	store "github.com/cosmos/cosmos-sdk/store/types"
 )
 
-const UpgradeName = "v1.1.0"
+const UpgradeName = "v2.0.0"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
