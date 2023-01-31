bfbaa863db5f90ddecbdacac465c8d24c9bfd875
Paweł Kulpa <pawel.kulpa@ovoo.pl>
2023-01-31 17:08:35 +0100

    Update upgrade handler, add store upgrade.

--- app/upgrades/v200/constants.go
+++ app/upgrades/v200/constants.go
@@ -2,6 +2,7 @@ package v200
 
 import (
 	"github.com/chain4energy/c4e-chain/app/upgrades"
+	cfeairdropmoduletypes "github.com/chain4energy/c4e-chain/x/cfeairdrop/types"
 	store "github.com/cosmos/cosmos-sdk/store/types"
 )
 
@@ -10,5 +11,9 @@ const UpgradeName = "v2.0.0"
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
-	StoreUpgrades:        store.StoreUpgrades{},
+	StoreUpgrades: store.StoreUpgrades{
+		Added: []string{
+			cfeairdropmoduletypes.ModuleName,
+		},
+	},
 }
