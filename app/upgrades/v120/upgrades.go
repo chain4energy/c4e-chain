d1062408f5df50ee6b8715be4cd62231590db995
Dawid Kruk <dawid.kruk@ovoo.pl>
2023-03-30 12:56:31 +0200

    Add ica controller and ica host upgrade params configuration

--- app/upgrades/v120/upgrades.go
+++ app/upgrades/v120/upgrades.go
@@ -1,6 +1,7 @@
 package v120
 
 import (
+	"fmt"
 	"github.com/chain4energy/c4e-chain/app/upgrades"
 	cfeupgradetypes "github.com/chain4energy/c4e-chain/app/upgrades"
 	cfedistributormoduletypes "github.com/chain4energy/c4e-chain/x/cfedistributor/types"
@@ -11,6 +12,10 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	ica "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts"
+	icacontrollertypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/controller/types"
+	icahosttypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/host/types"
+	icatypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/types"
 )
 
 func CreateUpgradeHandler(
@@ -20,6 +25,30 @@ func CreateUpgradeHandler(
 	appKeepers cfeupgradetypes.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		mod, found := mm.Modules[icatypes.ModuleName]
+		if !found {
+			panic(fmt.Sprintf("module %s is not in the module manager", icatypes.ModuleName))
+		}
+
+		icaMod, ok := mod.(ica.AppModule)
+		if !ok {
+			panic(fmt.Sprintf("expected module %s to be type %T, got %T", icatypes.ModuleName, ica.AppModule{}, mod))
+		}
+
+		// Add Interchain Accounts host module
+		// set the ICS27 consensus version so InitGenesis is not run
+		vm[icatypes.ModuleName] = icaMod.ConsensusVersion()
+
+		controllerParams := icacontrollertypes.Params{
+			ControllerEnabled: false,
+		}
+		hostParams := icahosttypes.Params{
+			HostEnabled:   false,
+			AllowMessages: []string{},
+		}
+
+		icaMod.InitModule(ctx, controllerParams, hostParams)
+
 		for _, subspace := range appKeepers.GetParamKeeper().GetSubspaces() {
 			subspace := subspace
 
