7839b141f772f91f7b969cab5897693ab8b2482c
Dawid Kruk <dawid.kruk@ovoo.pl>
2023-04-06 10:46:03 +0200

    Delete unnecessary third party files

--- types/errors/errors.go
+++ types/errors/errors.go
@@ -1,7 +1,7 @@
 package types
 
 import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"cosmossdk.io/errors"
 )
 
 // C4eCodespace is the codespace for all commen C4E errors
@@ -9,12 +9,12 @@ const C4eCodespace = "c4e"
 
 // x/cfeairdrop module sentinel errors
 var (
-	ErrAmount                          = sdkerrors.Register(C4eCodespace, 2, "wrong amount value")
-	ErrAlreadyExists                   = sdkerrors.Register(C4eCodespace, 3, "entity already exists")
-	ErrSendCoins                       = sdkerrors.Register(C4eCodespace, 4, "failed to send coins")
-	ErrAccountNotAllowedToReceiveFunds = sdkerrors.Register(C4eCodespace, 5, "account is not allowed to receive")
-	ErrInvalidAccountType              = sdkerrors.Register(C4eCodespace, 6, "invalid account type")
-	ErrParsing                         = sdkerrors.Register(C4eCodespace, 7, "failed to parse")
-	ErrParam                           = sdkerrors.Register(C4eCodespace, 8, "wrong param value")
-	ErrNotExists                       = sdkerrors.Register(C4eCodespace, 9, "entity does not exist")
+	ErrAmount                          = errors.Register(C4eCodespace, 2, "wrong amount value")
+	ErrAlreadyExists                   = errors.Register(C4eCodespace, 3, "entity already exists")
+	ErrSendCoins                       = errors.Register(C4eCodespace, 4, "failed to send coins")
+	ErrAccountNotAllowedToReceiveFunds = errors.Register(C4eCodespace, 5, "account is not allowed to receive")
+	ErrInvalidAccountType              = errors.Register(C4eCodespace, 6, "invalid account type")
+	ErrParsing                         = errors.Register(C4eCodespace, 7, "failed to parse")
+	ErrParam                           = errors.Register(C4eCodespace, 8, "wrong param value")
+	ErrNotExists                       = errors.Register(C4eCodespace, 9, "entity does not exist")
 )
--- x/cfevesting/types/errors.go
+++ x/cfevesting/types/errors.go
@@ -3,18 +3,18 @@ package types
 // DONTCOVER
 
 import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"cosmossdk.io/errors"
 )
 
 // x/cfevesting module sentinel errors
 var (
-	ErrAmount                          = sdkerrors.Register(ModuleName, 2, "wrong amount value")
-	ErrAlreadyExists                   = sdkerrors.Register(ModuleName, 3, "entity already exists")
-	ErrSendCoins                       = sdkerrors.Register(ModuleName, 4, "failed to send coins")
-	ErrIdenticalAccountsAddresses      = sdkerrors.Register(ModuleName, 5, "account addresses cannot be identical")
-	ErrGetVestingType                  = sdkerrors.Register(ModuleName, 6, "failed to get vesting type")
-	ErrAccountNotAllowedToReceiveFunds = sdkerrors.Register(ModuleName, 7, "account is not allowed to receive")
-	ErrInvalidAccountType              = sdkerrors.Register(ModuleName, 8, "invalid account type")
-	ErrParsing                         = sdkerrors.Register(ModuleName, 9, "failed to parse")
-	ErrParam                           = sdkerrors.Register(ModuleName, 10, "wrong param value")
+	ErrAmount                          = errors.Register(ModuleName, 2, "wrong amount value")
+	ErrAlreadyExists                   = errors.Register(ModuleName, 3, "entity already exists")
+	ErrSendCoins                       = errors.Register(ModuleName, 4, "failed to send coins")
+	ErrIdenticalAccountsAddresses      = errors.Register(ModuleName, 5, "account addresses cannot be identical")
+	ErrGetVestingType                  = errors.Register(ModuleName, 6, "failed to get vesting type")
+	ErrAccountNotAllowedToReceiveFunds = errors.Register(ModuleName, 7, "account is not allowed to receive")
+	ErrInvalidAccountType              = errors.Register(ModuleName, 8, "invalid account type")
+	ErrParsing                         = errors.Register(ModuleName, 9, "failed to parse")
+	ErrParam                           = errors.Register(ModuleName, 10, "wrong param value")
 )
