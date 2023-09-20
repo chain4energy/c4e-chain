4a4caf1592305e6f19ce08d121f6ce6f6146eca4
Dawid Kruk <dawid.kruk@ovoo.pl>
2023-09-20 18:54:56 +0200

    Bump wasmd version to 0.42.0, simulation fix in progress

--- app/simulation_test.go
+++ app/simulation_test.go
@@ -1,29 +1,22 @@
-package app_test
+package app
 
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/CosmWasm/wasmd/app"
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
-	"github.com/chain4energy/c4e-chain/v2/app"
-	"math/rand"
-	"os"
-	"runtime/debug"
-	"strings"
-	"testing"
-	"time"
-
+	cfeclaimtypes "github.com/chain4energy/c4e-chain/v2/x/cfeclaim/types"
+	cfedistributortypes "github.com/chain4energy/c4e-chain/v2/x/cfedistributor/types"
+	cfemintertypes "github.com/chain4energy/c4e-chain/v2/x/cfeminter/types"
+	cfevestingtypes "github.com/chain4energy/c4e-chain/v2/x/cfevesting/types"
 	dbm "github.com/cometbft/cometbft-db"
-	abci "github.com/cometbft/cometbft/abci/types"
-	"github.com/cometbft/cometbft/libs/log"
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
-	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/server"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	simulationtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	authzkeeper "github.com/cosmos/cosmos-sdk/x/authz/keeper"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
@@ -31,342 +24,97 @@ import (
 	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	evidencetypes "github.com/cosmos/cosmos-sdk/x/evidence/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
-	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
-	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 	simcli "github.com/cosmos/cosmos-sdk/x/simulation/client/cli"
 	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	ibctransfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+
+	simulationtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 )
 
 var emptyWasmOpts []wasmkeeper.Option
-
-type storeKeysPrefixes struct {
-	A        storetypes.StoreKey
-	B        storetypes.StoreKey
-	Prefixes [][]byte
-}
 
 // Get flags every time the simulator is run
 func init() {
 	simcli.GetSimulatorFlags()
 }
 
-// fauxMerkleModeOpt returns a BaseApp option to use a dbStoreAdapter instead of
-// an IAVLStore for faster simulation speed.
-func fauxMerkleModeOpt(bapp *baseapp.BaseApp) {
-	bapp.SetFauxMerkleMode()
+type StoreKeysPrefixes struct {
+	A        storetypes.StoreKey
+	B        storetypes.StoreKey
+	Prefixes [][]byte
 }
 
 // BenchmarkSimulation run the chain simulation
-// Running using starport command:
-// `starport chain simulate -v --numBlocks 200 --blockSize 50`
 // Running as go benchmark test:
-// `go test -benchmem -run=^$ -bench ^BenchmarkSimulation ./app -NumBlocks=200 -BlockSize 50 -Commit=true -Verbose=true -Enabled=true`
 func BenchmarkSimulation(b *testing.B) {
-	simcli.FlagSeedValue = time.Now().Unix()
-	simcli.FlagVerboseValue = true
-	simcli.FlagCommitValue = true
-	simcli.FlagEnabledValue = true
-
-	config := simcli.NewConfigFromFlags()
-	config.ChainID = "mars-simapp"
-	db, dir, logger, _, err := simtestutil.SetupSimulation(
-		config,
-		"leveldb-bApp-sim",
-		"Simulation",
-		simcli.FlagVerboseValue,
-		simcli.FlagEnabledValue,
-	)
-	require.NoError(b, err, "simulation setup failed")
-
-	b.Cleanup(func() {
-		require.NoError(b, db.Close())
-		require.NoError(b, os.RemoveAll(dir))
-	})
-
-	appOptions := make(simtestutil.AppOptionsMap, 0)
-	appOptions[flags.FlagHome] = app.DefaultNodeHome
-	appOptions[server.FlagInvCheckPeriod] = simcli.FlagPeriodValue
-
-	bApp := app.New(
-		logger,
-		db,
-		nil,
-		true,
-		wasmtypes.EnableAllProposals,
-		map[int64]bool{},
-		app.DefaultNodeHome,
-		0,
-		app.MakeEncodingConfig(),
-		appOptions,
-		emptyWasmOpts,
-		baseapp.SetChainID(config.ChainID),
-	)
-	require.Equal(b, app.Name, bApp.Name())
-
-	// run randomized simulation
-	_, simParams, simErr := simulation.SimulateFromSeed(
-		b,
-		os.Stdout,
-		bApp.BaseApp,
-		simtestutil.AppStateFn(
-			bApp.AppCodec(),
-			bApp.SimulationManager(),
-			app.NewDefaultGenesisState(bApp.AppCodec()),
-		),
-		simulationtypes.RandomAccounts,
-		simtestutil.SimulationOperations(bApp, bApp.AppCodec(), config),
-		bApp.ModuleAccountAddrs(),
-		config,
-		bApp.AppCodec(),
-	)
-
-	// export state and simParams before the simulation error is checked
-	err = simtestutil.CheckExportSimulation(bApp, config, simParams)
-	require.NoError(b, err)
-	require.NoError(b, simErr)
-
-	if config.Commit {
-		simtestutil.PrintStats(db)
-	}
-}
-
-func TestAppStateDeterminism(t *testing.T) {
-	if !simcli.FlagEnabledValue {
-		t.Skip("skipping application simulation")
-	}
-
-	config := simcli.NewConfigFromFlags()
-	config.InitialBlockHeight = 1
-	config.ExportParamsPath = ""
-	config.OnOperation = true
-	config.AllInvariants = true
-
-	var (
-		r                    = rand.New(rand.NewSource(time.Now().Unix()))
-		numSeeds             = 3
-		numTimesToRunPerSeed = 5
-		appHashList          = make([]json.RawMessage, numTimesToRunPerSeed)
-		appOptions           = make(simtestutil.AppOptionsMap, 0)
-	)
-	appOptions[flags.FlagHome] = app.DefaultNodeHome
-	appOptions[server.FlagInvCheckPeriod] = simcli.FlagPeriodValue
-
-	for i := 0; i < numSeeds; i++ {
-		config.Seed = r.Int63()
-
-		for j := 0; j < numTimesToRunPerSeed; j++ {
-			var logger log.Logger
-			if simcli.FlagVerboseValue {
-				logger = log.TestingLogger()
-			} else {
-				logger = log.NewNopLogger()
-			}
-			chainID := fmt.Sprintf("chain-id-%d-%d", i, j)
-			config.ChainID = chainID
-
-			db := dbm.NewMemDB()
-			bApp := app.New(
-				logger,
-				db,
-				nil,
-				true,
-				wasmtypes.EnableAllProposals,
-				map[int64]bool{},
-				app.DefaultNodeHome,
-				simcli.FlagPeriodValue,
-				app.MakeEncodingConfig(),
-				appOptions,
-				emptyWasmOpts,
-				fauxMerkleModeOpt,
-				baseapp.SetChainID(chainID),
-			)
-
-			fmt.Printf(
-				"running non-determinism simulation; seed %d: %d/%d, attempt: %d/%d\n",
-				config.Seed, i+1, numSeeds, j+1, numTimesToRunPerSeed,
-			)
-
-			_, _, err := simulation.SimulateFromSeed(
-				t,
-				os.Stdout,
-				bApp.BaseApp,
-				simtestutil.AppStateFn(
-					bApp.AppCodec(),
-					bApp.SimulationManager(),
-					app.NewDefaultGenesisState(bApp.AppCodec()),
-				),
-				simulationtypes.RandomAccounts,
-				simtestutil.SimulationOperations(bApp, bApp.AppCodec(), config),
-				bApp.ModuleAccountAddrs(),
-				config,
-				bApp.AppCodec(),
-			)
-			require.NoError(t, err)
-
-			if config.Commit {
-				simtestutil.PrintStats(db)
-			}
-
-			appHash := bApp.LastCommitID().Hash
-			appHashList[j] = appHash
-
-			if j != 0 {
-				require.Equal(
-					t, string(appHashList[0]), string(appHashList[j]),
-					"non-determinism in seed %d: %d/%d, attempt: %d/%d\n", config.Seed, i+1, numSeeds, j+1, numTimesToRunPerSeed,
-				)
-			}
-		}
-	}
-}
-
-func TestAppImportExport(t *testing.T) {
-	config := simcli.NewConfigFromFlags()
-	config.ChainID = "mars-simapp-import"
-
-	db, dir, logger, skip, err := simtestutil.SetupSimulation(
-		config,
-		"leveldb-app-sim",
-		"Simulation",
-		simcli.FlagVerboseValue,
-		simcli.FlagEnabledValue,
-	)
-	if skip {
-		t.Skip("skipping application import/export simulation")
-	}
-	require.NoError(t, err, "simulation setup failed")
+	_, _, cleanup := setupSimulation(b, "goleveldb-app-sim", "Simulation")
 
 	defer func() {
-		require.NoError(t, db.Close())
-		require.NoError(t, os.RemoveAll(dir))
+		cleanup()
 	}()
+}
 
-	appOptions := make(simtestutil.AppOptionsMap, 0)
-	appOptions[flags.FlagHome] = app.DefaultNodeHome
-	appOptions[server.FlagInvCheckPeriod] = simcli.FlagPeriodValue
-
-	bApp := app.New(
-		logger,
-		db,
-		nil,
-		true,
-		wasmtypes.EnableAllProposals,
-		map[int64]bool{},
-		app.DefaultNodeHome,
-		0,
-		app.MakeEncodingConfig(),
-		appOptions,
-		emptyWasmOpts,
-		baseapp.SetChainID(config.ChainID),
-	)
-	require.Equal(t, app.Name, bApp.Name())
-
-	// run randomized simulation
-	_, simParams, simErr := simulation.SimulateFromSeed(
-		t,
-		os.Stdout,
-		bApp.BaseApp,
-		simtestutil.AppStateFn(
-			bApp.AppCodec(),
-			bApp.SimulationManager(),
-			app.NewDefaultGenesisState(bApp.AppCodec()),
-		),
-		simulationtypes.RandomAccounts,
-		simtestutil.SimulationOperations(bApp, bApp.AppCodec(), config),
-		bApp.BlockedModuleAccountAddrs(),
-		config,
-		bApp.AppCodec(),
-	)
-	require.NoError(t, simErr)
-
-	// export state and simParams before the simulation error is checked
-	err = simtestutil.CheckExportSimulation(bApp, config, simParams)
-	require.NoError(t, err)
-
-	if config.Commit {
-		simtestutil.PrintStats(db)
-	}
+func BenchmarkSimTest(b *testing.B) {
+	app, _, cleanup1 := setupSimulation(b, "goleveldb-app-sim", "Simulation")
 
 	fmt.Printf("exporting genesis...\n")
-
-	exported, err := bApp.ExportAppStateAndValidators(false, []string{}, []string{})
-	require.NoError(t, err)
+	exported, err := app.ExportAppStateAndValidators(false, []string{}, []string{})
+	require.NoError(b, err)
 
 	fmt.Printf("importing genesis...\n")
 
-	newDB, newDir, _, _, err := simtestutil.SetupSimulation(
-		config,
-		"leveldb-app-sim-2",
-		"Simulation-2",
-		simcli.FlagVerboseValue,
-		simcli.FlagEnabledValue,
-	)
-	require.NoError(t, err, "simulation setup failed")
+	var genesisState GenesisState
 
-	defer func() {
-		require.NoError(t, newDB.Close())
-		require.NoError(t, os.RemoveAll(newDir))
-	}()
-
-	newApp := app.New(
-		log.NewNopLogger(),
-		newDB,
-		nil,
-		true,
-		wasmtypes.EnableAllProposals,
-		map[int64]bool{},
-		app.DefaultNodeHome,
-		0,
-		app.MakeEncodingConfig(),
-		appOptions,
-		emptyWasmOpts,
-		baseapp.SetChainID(config.ChainID),
-	)
-	require.Equal(t, app.Name, bApp.Name())
-
-	var genesisState app.GenesisState
 	err = json.Unmarshal(exported.AppState, &genesisState)
-	require.NoError(t, err)
+	require.NoError(b, err)
+
+	newApp, _, _, _, _, cleanup2 := BaseSimulationSetup(b, "goleveldb-app-sim-2", "Simulation-2")
 
 	defer func() {
-		if r := recover(); r != nil {
-			err := fmt.Sprintf("%v", r)
-			if !strings.Contains(err, "validator set is empty after InitGenesis") {
-				panic(r)
-			}
-			logger.Info("Skipping simulation as all validators have been unbonded")
-			logger.Info("err", err, "stacktrace", string(debug.Stack()))
-		}
+		cleanup1()
+		cleanup2()
 	}()
 
-	ctxA := bApp.NewContext(true, tmproto.Header{Height: bApp.LastBlockHeight()})
-	ctxB := newApp.NewContext(true, tmproto.Header{Height: bApp.LastBlockHeight()})
-	newApp.ModuleManager().InitGenesis(ctxB, bApp.AppCodec(), genesisState)
+	ctxA := app.NewContext(true, tmproto.Header{Height: app.LastBlockHeight()})
+	ctxB := newApp.NewContext(true, tmproto.Header{Height: app.LastBlockHeight()})
+
+	newApp.mm.InitGenesis(ctxB, app.AppCodec(), genesisState)
 	newApp.StoreConsensusParams(ctxB, exported.ConsensusParams)
 
 	fmt.Printf("comparing stores...\n")
 
-	storeKeysPrefixes := []storeKeysPrefixes{
-		{bApp.GetKey(authtypes.StoreKey), newApp.GetKey(authtypes.StoreKey), [][]byte{}},
+	storeKeysPrefixes := []StoreKeysPrefixes{
+		{app.keys[authtypes.StoreKey], newApp.keys[authtypes.StoreKey], [][]byte{}},
 		{
-			bApp.GetKey(stakingtypes.StoreKey), newApp.GetKey(stakingtypes.StoreKey),
+			app.keys[stakingtypes.StoreKey], newApp.keys[stakingtypes.StoreKey],
 			[][]byte{
 				stakingtypes.UnbondingQueueKey, stakingtypes.RedelegationQueueKey, stakingtypes.ValidatorQueueKey,
-				stakingtypes.HistoricalInfoKey, stakingtypes.UnbondingIDKey, stakingtypes.UnbondingIndexKey, stakingtypes.UnbondingTypeKey, stakingtypes.ValidatorUpdatesKey,
+				stakingtypes.HistoricalInfoKey,
 			},
-		}, // ordering may change but it doesn't matter
-		{bApp.GetKey(slashingtypes.StoreKey), newApp.GetKey(slashingtypes.StoreKey), [][]byte{}},
-		{bApp.GetKey(minttypes.StoreKey), newApp.GetKey(minttypes.StoreKey), [][]byte{}},
-		{bApp.GetKey(distrtypes.StoreKey), newApp.GetKey(distrtypes.StoreKey), [][]byte{}},
-		{bApp.GetKey(banktypes.StoreKey), newApp.GetKey(banktypes.StoreKey), [][]byte{banktypes.BalancesPrefix}},
-		{bApp.GetKey(paramstypes.StoreKey), newApp.GetKey(paramstypes.StoreKey), [][]byte{}},
-		{bApp.GetKey(govtypes.StoreKey), newApp.GetKey(govtypes.StoreKey), [][]byte{}},
-		{bApp.GetKey(evidencetypes.StoreKey), newApp.GetKey(evidencetypes.StoreKey), [][]byte{}},
-		{bApp.GetKey(capabilitytypes.StoreKey), newApp.GetKey(capabilitytypes.StoreKey), [][]byte{}},
-		{bApp.GetKey(authzkeeper.StoreKey), newApp.GetKey(authzkeeper.StoreKey), [][]byte{authzkeeper.GrantKey, authzkeeper.GrantQueuePrefix}},
+		},
+		{app.keys[slashingtypes.StoreKey], newApp.keys[slashingtypes.StoreKey], [][]byte{}},
+		{app.keys[distrtypes.StoreKey], newApp.keys[distrtypes.StoreKey], [][]byte{}},
+		{app.keys[banktypes.StoreKey], newApp.keys[banktypes.StoreKey], [][]byte{banktypes.BalancesPrefix}},
+		{app.keys[paramtypes.StoreKey], newApp.keys[paramtypes.StoreKey], [][]byte{}},
+		{app.keys[govtypes.StoreKey], newApp.keys[govtypes.StoreKey], [][]byte{}},
+		{app.keys[evidencetypes.StoreKey], newApp.keys[evidencetypes.StoreKey], [][]byte{}},
+		{app.keys[capabilitytypes.StoreKey], newApp.keys[capabilitytypes.StoreKey], [][]byte{}},
+		{app.keys[authzkeeper.StoreKey], newApp.keys[authzkeeper.StoreKey], [][]byte{authzkeeper.GrantKey, authzkeeper.GrantQueuePrefix}},
+
+		// IBC
+		{app.keys[ibctransfertypes.StoreKey], newApp.keys[ibctransfertypes.StoreKey], [][]byte{}},
+
+		// OUR MODULES
+		{app.keys[cfevestingtypes.StoreKey], newApp.keys[cfevestingtypes.StoreKey], [][]byte{}},
+		{app.keys[cfedistributortypes.StoreKey], newApp.keys[cfedistributortypes.StoreKey], [][]byte{}},
+		{app.keys[cfemintertypes.StoreKey], newApp.keys[cfemintertypes.StoreKey], [][]byte{}},
+		{app.keys[cfeclaimtypes.StoreKey], newApp.keys[cfeclaimtypes.StoreKey], [][]byte{}},
 	}
 
 	for _, skp := range storeKeysPrefixes {
@@ -374,143 +122,84 @@ func TestAppImportExport(t *testing.T) {
 		storeB := ctxB.KVStore(skp.B)
 
 		failedKVAs, failedKVBs := sdk.DiffKVStores(storeA, storeB, skp.Prefixes)
-		require.Equal(t, len(failedKVAs), len(failedKVBs), "unequal sets of key-values to compare")
+		require.Equal(b, len(failedKVAs), len(failedKVBs), "unequal sets of key-values to compare")
 
 		fmt.Printf("compared %d different key/value pairs between %s and %s\n", len(failedKVAs), skp.A, skp.B)
-		require.Equal(t, 0, len(failedKVAs), simtestutil.GetSimulationLog(skp.A.Name(), bApp.SimulationManager().StoreDecoders, failedKVAs, failedKVBs))
+		simLog := simtestutil.GetSimulationLog(
+			skp.A.Name(),
+			app.SimulationManager().StoreDecoders,
+			failedKVAs,
+			failedKVBs,
+		)
+		require.Equal(b, len(failedKVAs), 0, simLog)
 	}
 }
 
-func TestAppSimulationAfterImport(t *testing.T) {
-	config := simcli.NewConfigFromFlags()
-	config.ChainID = "mars-simapp-after-import"
+func setupSimulation(tb testing.TB, dirPrevix string, dbName string) (c4eapp2 *App, simParams simulation.Params, cleanup func()) {
+	c4eapp, _, config, db, _, cleanup := BaseSimulationSetup(tb, dirPrevix, dbName)
 
-	db, dir, logger, skip, err := simtestutil.SetupSimulation(
-		config,
-		"leveldb-app-sim",
-		"Simulation",
-		simcli.FlagVerboseValue,
-		simcli.FlagEnabledValue,
-	)
-	if skip {
-		t.Skip("skipping application simulation after import")
-	}
-	require.NoError(t, err, "simulation setup failed")
-
+	weightedOperations := simtestutil.SimulationOperations(c4eapp, c4eapp.AppCodec(), config)
 	defer func() {
-		require.NoError(t, db.Close())
-		require.NoError(t, os.RemoveAll(dir))
+		if r := recover(); r != nil {
+			cleanup()
+		}
 	}()
+	_, simParams, simErr := simulation.SimulateFromSeed(
+		tb,
+		os.Stdout,
+		c4eapp.BaseApp,
+		simtestutil.AppStateFn(
+			c4eapp.AppCodec(),
+			c4eapp.SimulationManager(),
+			app.NewDefaultGenesisState(c4eapp.AppCodec()),
+		),
+		simulationtypes.RandomAccounts,
+		weightedOperations,
+		app.ModuleAccountAddrs(),
+		config,
+		c4eapp.AppCodec(),
+	)
+	require.NoError(tb, simErr)
+
+	err := simtestutil.CheckExportSimulation(c4eapp, config, simParams)
+
+	require.NoError(tb, err)
+	if config.Commit {
+		simtestutil.PrintStats(db)
+	}
+
+	return c4eapp, simParams, cleanup
+}
+
+func BaseSimulationSetup(tb testing.TB, dirPrevix string, dbName string) (*App, GenesisState, simulationtypes.Config, dbm.DB, string, func()) {
+	config := simcli.NewConfigFromFlags()
+	config.ChainID = "c4e-chain"
+	db, dir, logger, _, err := simtestutil.SetupSimulation(config, dirPrevix, dbName, simcli.FlagVerboseValue, simcli.FlagEnabledValue)
+	require.NoError(tb, err, "simulation setup failed")
+
+	encoding := MakeEncodingConfig()
 
 	appOptions := make(simtestutil.AppOptionsMap, 0)
 	appOptions[flags.FlagHome] = app.DefaultNodeHome
 	appOptions[server.FlagInvCheckPeriod] = simcli.FlagPeriodValue
-
-	bApp := app.New(
+	c4eapp := New(
 		logger,
 		db,
 		nil,
 		true,
 		wasmtypes.EnableAllProposals,
 		map[int64]bool{},
-		app.DefaultNodeHome,
-		0,
-		app.MakeEncodingConfig(),
+		DefaultNodeHome,
+		simcli.FlagPeriodValue,
+		encoding,
 		appOptions,
 		emptyWasmOpts,
-		fauxMerkleModeOpt,
-		baseapp.SetChainID(config.ChainID),
 	)
-	require.Equal(t, app.Name, bApp.Name())
-
-	// run randomized simulation
-	stopEarly, simParams, simErr := simulation.SimulateFromSeed(
-		t,
-		os.Stdout,
-		bApp.BaseApp,
-		simtestutil.AppStateFn(
-			bApp.AppCodec(),
-			bApp.SimulationManager(),
-			app.NewDefaultGenesisState(bApp.AppCodec()),
-		),
-		simulationtypes.RandomAccounts,
-		simtestutil.SimulationOperations(bApp, bApp.AppCodec(), config),
-		bApp.BlockedModuleAccountAddrs(),
-		config,
-		bApp.AppCodec(),
-	)
-	require.NoError(t, simErr)
-
-	// export state and simParams before the simulation error is checked
-	err = simtestutil.CheckExportSimulation(bApp, config, simParams)
-	require.NoError(t, err)
-
-	if config.Commit {
-		simtestutil.PrintStats(db)
+	genesisState := NewDefaultGenesisState(encoding.Codec)
+	cleanup := func() {
+		require.NoError(tb, db.Close())
+		require.NoError(tb, os.RemoveAll(dir))
 	}
 
-	if stopEarly {
-		fmt.Println("can't export or import a zero-validator genesis, exiting test...")
-		return
-	}
-
-	fmt.Printf("exporting genesis...\n")
-
-	exported, err := bApp.ExportAppStateAndValidators(true, []string{}, []string{})
-	require.NoError(t, err)
-
-	fmt.Printf("importing genesis...\n")
-
-	newDB, newDir, _, _, err := simtestutil.SetupSimulation(
-		config,
-		"leveldb-app-sim-2",
-		"Simulation-2",
-		simcli.FlagVerboseValue,
-		simcli.FlagEnabledValue,
-	)
-	require.NoError(t, err, "simulation setup failed")
-
-	defer func() {
-		require.NoError(t, newDB.Close())
-		require.NoError(t, os.RemoveAll(newDir))
-	}()
-
-	newApp := app.New(
-		log.NewNopLogger(),
-		newDB,
-		nil,
-		true,
-		wasmtypes.EnableAllProposals,
-		map[int64]bool{},
-		app.DefaultNodeHome,
-		0,
-		app.MakeEncodingConfig(),
-		appOptions,
-		emptyWasmOpts,
-		fauxMerkleModeOpt,
-		baseapp.SetChainID(config.ChainID),
-	)
-	require.Equal(t, app.Name, bApp.Name())
-
-	newApp.InitChain(abci.RequestInitChain{
-		ChainId:       config.ChainID,
-		AppStateBytes: exported.AppState,
-	})
-
-	_, _, err = simulation.SimulateFromSeed(
-		t,
-		os.Stdout,
-		newApp.BaseApp,
-		simtestutil.AppStateFn(
-			bApp.AppCodec(),
-			bApp.SimulationManager(),
-			app.NewDefaultGenesisState(bApp.AppCodec()),
-		),
-		simulationtypes.RandomAccounts,
-		simtestutil.SimulationOperations(newApp, newApp.AppCodec(), config),
-		newApp.BlockedModuleAccountAddrs(),
-		config,
-		bApp.AppCodec(),
-	)
-	require.NoError(t, err)
+	return c4eapp, genesisState, config, db, dir, cleanup
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	cosmossdk.io/errors v1.0.0
 	cosmossdk.io/math v1.1.2
 	cosmossdk.io/simapp v0.0.0-20230323161446-0af178d721ff
-	github.com/CosmWasm/wasmd v0.41.0
+	github.com/CosmWasm/wasmd v0.42.0
 	github.com/armon/go-metrics v0.4.1
 	github.com/cometbft/cometbft v0.37.2
 	github.com/cometbft/cometbft-db v0.8.0
@@ -15,7 +15,7 @@ require (
 	github.com/cosmos/cosmos-sdk v0.47.5
 	github.com/cosmos/go-bip39 v1.0.0
 	github.com/cosmos/gogoproto v1.4.10
-	github.com/cosmos/ibc-go/v7 v7.2.0
+	github.com/cosmos/ibc-go/v7 v7.3.0
 	github.com/gogo/protobuf v1.3.2
 	github.com/golang/protobuf v1.5.3
 	github.com/gorilla/mux v1.8.0
@@ -49,7 +49,7 @@ require (
 	github.com/99designs/keyring v1.2.1 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/ChainSafe/go-schnorrkel v0.0.0-20200405005733-88cbf1b4c40d // indirect
-	github.com/CosmWasm/wasmvm v1.3.0 // indirect
+	github.com/CosmWasm/wasmvm v1.4.0 // indirect
 	github.com/Microsoft/go-winio v0.6.0 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/aws/aws-sdk-go v1.44.203 // indirect
@@ -70,7 +70,7 @@ require (
 	github.com/containerd/continuity v0.3.0 // indirect
 	github.com/cosmos/btcutil v1.0.5 // indirect
 	github.com/cosmos/gogogateway v1.2.0 // indirect
-	github.com/cosmos/iavl v0.20.0 // indirect
+	github.com/cosmos/iavl v0.20.1 // indirect
 	github.com/cosmos/ics23/go v0.10.0 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.12.1 // indirect
 	github.com/cosmos/rosetta-sdk-go v0.10.0 // indirect
